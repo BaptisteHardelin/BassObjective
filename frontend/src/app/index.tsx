5156a55aaad84c84ed326150801e06c8b8e75946
BaptisteHardelin <baptiste.hardelin@gmail.com>
2026-06-25 21:42:23 +0200

    feat: add header

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View, Text } from 'react-native';
 
-export default function App() {
+export default function HomeScreen() {
   return (
     <View>
-      <Text>Hello, World!</Text>
+      
     </View>
   );
 }
---
+++ frontend/src/app/kabanColumn.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+
+const KabanColumn= () => {
+   return <View><Text>kanban column</Text></View>
+}
+
+
+export default KabanColumn;
